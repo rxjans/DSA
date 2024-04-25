80c79179c4e6b4dadfa3a18aa80ec402fb68c002
rxjans <120579803+rxjans@users.noreply.github.com>
2024-04-25 13:08:23 +0530

    Arrays: binary search

---
+++ Arrays/src/binary_search.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class binary_search {
+
+    public static int majority(int[] arr, int k) {
+        int high = arr.length -1;
+        int low = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] == k){
+                return mid;
+            }
+            else if(arr[mid]>k){
+                high -= 1;
+            }
+            else low += 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Specify Array Length: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter elements in your array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the element to be searched: ");
+        int k = sc.nextInt();
+        if(majority(arr, k) != -1){
+            System.out.println(majority(arr, k));
+        }
+
+    }
+
+}
