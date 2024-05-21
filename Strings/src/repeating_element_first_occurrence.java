19f7294c3f51a5f032e75d5b3b750f25c4a60f4f
rxjans <120579803+rxjans@users.noreply.github.com>
2024-05-21 13:44:40 +0530

    Strings: left most repeating element

---
+++ Strings/src/repeating_element_first_occurrence.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class repeating_element_first_occurrence {
+    static final int CHAR = 256;
+    public static int occurrence(String k){
+        int[] count = new int[CHAR];
+        for(int i=0; i<k.length(); i++){
+            count[k.charAt(i)]++;
+        }
+        for(int i=0; i<count.length; i++){
+            if(count[k.charAt(i)] > 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String k = sc.nextLine();
+        System.out.println("Index of repeating element: " + occurrence(k));
+    }
+
+}
