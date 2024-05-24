18db7a4e7a9bbd9212650f34702e87eb343a4d20
rxjans <120579803+rxjans@users.noreply.github.com>
2024-05-24 19:48:16 +0530

    Strings: pattern searching naive

---
+++ Strings/src/pattern_searching_naive.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class pattern_searching_naive {
+
+    public static void search(String str, String sub){
+        int n = str.length();
+        int m = sub.length();
+        for(int i=0; i<=n-m; i++){
+            int j =0;
+            for(j=0; j<m; j++){
+                if(sub.charAt(j) != str.charAt(i+j)){
+                    break;
+                }
+            }
+            if(j == m){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+        System.out.println("Enter the pattern to be searched: ");
+        String sub = sc.nextLine();
+        search(str, sub);
+    }
+
+}
