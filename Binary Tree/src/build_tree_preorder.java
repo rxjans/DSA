644bac8d817a74a67fdc09ff009419a72d2eb052
rxjans <rajan4.sharma2001@gmail.com>
2024-06-30 18:47:24 +0530

    Binary Tree: K distance nodes from root

--- Binary Tree/src/build_tree_preorder.java
+++ Binary Tree/src/build_tree_preorder.java
@@ -56,6 +56,17 @@ public class build_tree_preorder {
         return Math.max(height(root.left), height(root.right)) + 1;
     }
 
+    static void printkdist(Node root, int k){
+        if(root == null){
+            return;
+        }
+        if(k == 0){
+            System.out.print(root.data + " ");
+        }
+        printkdist(root.left, k-1);
+        printkdist(root.right, k-1);
+    }
+
     public static void main(String[] args) {
         int[] nodes = {1, 2 , 4, -1,-1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
@@ -68,6 +79,8 @@ public class build_tree_preorder {
         System.out.print("\nPostorder traversal is: ");
         postorder(root);
         System.out.println("\nheight of tree: " + height(root));
+        System.out.print("k(2) distance nodes: ");
+        printkdist(root, 2);
     }
 
 }
