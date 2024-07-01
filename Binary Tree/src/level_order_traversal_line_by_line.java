fb9c9cb51ff5e6ef9d4a5d49331f824d03787b9e
rxjans <rajan4.sharma2001@gmail.com>
2024-07-01 16:51:19 +0530

    Binary Tree: level order traversal method 2

--- Binary Tree/src/level_order_traversal_line_by_line.java
+++ Binary Tree/src/level_order_traversal_line_by_line.java
@@ -1,7 +1,8 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class level_order_traversal {
+public class level_order_traversal_line_by_line {
+
     static class Node{
         int data;
         Node left;
@@ -33,11 +34,17 @@ public class level_order_traversal {
         if(root == null){
             return;
         }
-        Queue<Node> q = new LinkedList<Node>();
+        Queue<Node> q = new LinkedList<>();
         q.add(root);
-        while(!q.isEmpty()){
+        q.add(null);
+        while(q.size() > 1){
             Node curr = q.poll();
-            System.out.println(curr.data + " ");
+            if(curr == null){
+                System.out.println();
+                q.add(null);
+                continue;
+            }
+            System.out.print(curr.data + " ");
             if(curr.left != null){
                 q.add(curr.left);
             }
@@ -45,15 +52,37 @@ public class level_order_traversal {
                 q.add(curr.right);
             }
         }
+    }
 
+    static void printlevel2(Node root){
+        if(root == null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int count = q.size();
+            for(int i=0; i<count; i++){
+                Node curr = q.poll();
+                System.out.print(curr.data + " ");
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         int[] nodes = {1, 2 , 4, -1,-1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println("level order traversal of binary tree is ");
-        printlevel(root);
+//        System.out.println("level order traversal of binary tree is ");
+        printlevel2(root);
     }
+
 
 }
