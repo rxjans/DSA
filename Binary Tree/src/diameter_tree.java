9824873241e1530114fea5fcd0d3406df3a34c59
rxjans <rajan4.sharma2001@gmail.com>
2024-07-07 00:12:29 +0530

    Binary Tree: diameter of tree method 2

--- Binary Tree/src/diameter_tree.java
+++ Binary Tree/src/diameter_tree.java
@@ -46,11 +46,24 @@ public class diameter_tree {
         return Math.max(d1, Math.max(d2, d3));
     }
 
+    static int res = 0;
+
+    static int diameter2(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        res = Math.max(res, 1+lh+rh);
+        return 1 + Math.max(lh, rh);
+    }
+
     public static void main(String[] args) {
         int[] nodes = {1, 2 , 4, -1,-1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.print("diameter of tree: " + diameter(root));
+        diameter2(root);
+        System.out.print("diameter of tree: " + res);
     }
 
 
