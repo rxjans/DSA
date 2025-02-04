273d9c8938077b323bd7dc780b3bdd796bda25f5
Rajan Sharma <rajan4.sharma2001@gmail.com>
2025-02-04 20:07:16 +0530

    Arrays: Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ Arrays/src/Longest_strictly_increasing_decreasing_arr.java
@@ -0,0 +1,25 @@
+public class Longest_strictly_increasing_decreasing_arr {
+    class Solution {
+        public int longestMonotonicSubarray(int[] nums) {
+            int inc = 1;
+            int dec = 1;
+            int max = 1;
+            for(int i = 1; i<nums.length; i++){
+                if(nums[i]>nums[i-1]){
+                    inc++;
+                    dec = 1;
+                }
+                else if(nums[i]<nums[i-1]){
+                    dec++;
+                    inc = 1;
+                }
+                else {
+                    inc = 1;
+                    dec = 1;
+                }
+                max = Math.max(max, Math.max(inc, dec));
+            }
+            return max;
+        }
+    }
+}
