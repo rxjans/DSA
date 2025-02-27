f8efb5ec792dfdfb68298efef17117b08cc35ca3
Rajan Sharma <rajan4.sharma2001@gmail.com>
2025-02-27 19:27:25 +0530

    Strings: 3174. Clear Digits Leetcode

---
+++ Strings/src/Clear_Digits.java
@@ -0,0 +1,15 @@
+public class Clear_Digits {
+    class Solution {
+        public String clearDigits(String s) {
+            StringBuilder sb = new StringBuilder();
+
+            for (final char c : s.toCharArray())
+                if (Character.isDigit(c))
+                    sb.setLength(sb.length() - 1);
+                else
+                    sb.append(c);
+
+            return sb.toString();
+        }
+    }
+}
